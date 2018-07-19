1eefffea9735b1fa9cc34c7836d486cfc7be5dd9
thaotonto <ots.hunter@gmail.com>
2018-07-19 14:44:29 +0700

    Chapter 8: Basic login

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem "uglifier", ">= 1.3.0"
 
 gem "coffee-rails", "~> 4.2"
 gem "jbuilder", "~> 2.5"
+gem "jquery-rails"
 gem "turbolinks", "~> 5"
 
 gem "bcrypt"
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,17 @@ class User < ApplicationRecord
     length: {minimum: Settings.user_setting.password_minimum_length}
   has_secure_password
 
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+  end
+
   private
   def downcase_email
     self.email = email.downcase
